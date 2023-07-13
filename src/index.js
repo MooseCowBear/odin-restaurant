c7aab8baa6b742dd8ebaa1fa047834278ed93de1
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-07-13 12:39:53 -0400

    add buttons for tabs, style about page

--- src/index.js
+++ src/index.js
@@ -2,18 +2,39 @@ import './style.css';
 
 console.log("The file has been bundled!");
 
-function component() {
-  const contentDiv = document.getElementById('content');
+function aboutComponent(parent) {
+  const component = document.createElement('div');
   const title = document.createElement('h1');
-
   title.textContent = 'Welcome to Toast.';
 
   const aboutDiv = document.createElement('div');
+  aboutDiv.classList.add("card");
   aboutDiv.innerHTML = `<p>Here at Toast we love many things, but mostly, <span>we love carbs. </span>
                       Try our curated selection, baked fresh daily!</p>`;
 
-  contentDiv.appendChild(title);
-  contentDiv.appendChild(aboutDiv);
+  component.appendChild(title);
+  component.appendChild(aboutDiv);
+
+  parent.appendChild(component);
 }
 
-component();
+function navComponent(parent) {
+  const tabs = document.createElement('nav');
+  const tabOptions = ["About", "Menu", "Contact"];
+  
+  for (const elem of tabOptions) {
+    const btn = document.createElement('button');
+    btn.textContent = elem;
+    btn.classList.add("tab");
+    tabs.appendChild(btn);
+  }
+  parent.appendChild(tabs);
+}
+
+function aboutSetUp() {
+  const contentDiv = document.getElementById('content');
+  navComponent(contentDiv);
+  aboutComponent(contentDiv);
+}
+
+aboutSetUp();
